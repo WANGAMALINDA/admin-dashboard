bff1b485da639a1a22f352c8e122347d1cef7bc7
Wanga Malinda <wangamalinda4@gmail.com>
2026-07-23 16:59:32 +0200

    handle Logout

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ function useIdleLogout(timeoutMs) {
     try {
       await supabase.auth.signOut();
     } finally {
-      navigate('/login');
+      navigate('/admin-dashboard');
     }
   }, [navigate]);
 
